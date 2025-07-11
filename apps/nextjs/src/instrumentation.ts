3100b624b7a94cf8d8518071171dfdcd4298148e
Squishmeist <ayelee02@gmail.com>
2025-07-11 18:00:56 +0100

    fix: otlp uses basic auth

--- apps/nextjs/src/instrumentation.ts
+++ apps/nextjs/src/instrumentation.ts
@@ -8,7 +8,7 @@ export function register() {
     traceExporter: new OTLPHttpJsonTraceExporter({
       url: env.TELEMETRY_TRACE_HOST,
       headers: {
-        Authorization: `Bearer ${env.TELEMETRY_TRACE_AUTH}`,
+        Authorization: `Basic ${env.TELEMETRY_TRACE_AUTH}`,
       },
     }),
   });
